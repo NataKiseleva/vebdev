e9b5e3466d26827be06d9911ab48690e35c90134
Nikita Login <nlogin@89-108-81-105.cloudvps.regruhosting.ru>
2021-12-23 15:54:15 +0300

    fixed api

--- api/service.py
+++ api/service.py
@@ -4,14 +4,20 @@ from fastapi import FastAPI, Query
 
 from typing import List, Optional
 
-from RST_search import rst_search
+from RST_search import rst_search, tags_abbs
+
+tags = [tag[1] for tag in tags_abbs]
 
 app = FastAPI()
 
 @app.get("/search")
 def search(
     input_query: str = Query("Германия"),
-    selected_tags: Optional[List[str]] = Query(None),
+    selected_tag: Optional[str] = Query(None, enum=tags),
     selected_rels: Optional[List[str]] = Query(None)
 ):
+    if selected_tag:
+        selected_tags = [selected_tag]
+    else:
+        selected_tags = None
     return rst_search(input_query, selected_tags, selected_rels)
