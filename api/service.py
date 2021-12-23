e0fcccc6037130fb63d0334d40f4fe4cf342f168
Nikita Login <nlogin@89-108-81-105.cloudvps.regruhosting.ru>
2021-12-23 16:26:14 +0300

    API docs text

--- api/service.py
+++ api/service.py
@@ -6,19 +6,26 @@ from typing import List, Optional
 
 from RST_search import rst_search, tags_abbs, rels_abbs
 
-tags = [tag[1] for tag in tags_abbs]
-rels = [f"<i>{tag[1]}</i>" for tag in rels_abbs]
+all_tags = [tag[1] for tag in tags_abbs]
+all_rels = [f"<i>{tag[1]}</i>" for tag in rels_abbs]
 
-app = FastAPI()
+app = FastAPI(
+    title = "RST corpus seach API",
+    version = '0.1.0'
+)
 
-@app.get("/search")
+@app.get(
+    "/search",
+    description="Using this method you can search for a specific Russian or English word in parallel RST corpus. You can select a POS tag for disambiguation and specify discource relations you would like to see on that word",
+    summary="Search a word in RST corpus"
+)
 def search(
-    input_query: str = Query("Германия", description="A Russian or English word you would like to search"),
-    selected_tag: Optional[str] = Query(None, enum=tags, description="(Optional) POS tag of the given word "),
-    selected_rels: Optional[List[str]] = Query(None, description=f"(Optional) Type of discourse relation - one or multiple from: {', '.join(rels)}")
+    query: str = Query("Германия", description="A Russian or English word you would like to search"),
+    tag: Optional[str] = Query(None, enum=all_tags, description="(Optional) POS tag of the given word "),
+    rels: Optional[List[str]] = Query(None, description=f"(Optional) Type of discourse relation - one or multiple from: {', '.join(all_rels)}")
 ):
-    if selected_tag:
-        selected_tags = [selected_tag]
+    if tag:
+        tags = [tag]
     else:
-        selected_tags = None
-    return rst_search(input_query, selected_tags, selected_rels)
+        tags = None
+    return rst_search(query, tags, rels)
