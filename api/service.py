ca82cc4e361c640227cb64f8faed280c57fb3b4f
nicklogin <niklogin10v@yandex.ru>
2021-12-23 14:52:30 +0300

    api updated

--- api/service.py
+++ api/service.py
@@ -1,18 +1,18 @@
 import os
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 from typing import List, Optional
 
-from RST_search import rst_search
+from RST_search import rst_search, tags_abbs, rels_abbs
 
 app = FastAPI()
 
-@app.post("/search")
+@app.get("/search")
 def search(
-    input_query: str = "Германия",
-    selected_tags: Optional[List[str]] = ["NOUN"],
-    selected_rels: Optional[List[str]] = ["contrast"]
+    input_query: str = Query("Германия"),
+    selected_tags: Optional[List[str]] = Query(None),
+    selected_rels: Optional[List[str]] = Query(None)
 ):
     print("ok")
     return rst_search(input_query, selected_tags, selected_rels)
